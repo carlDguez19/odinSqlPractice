d1e5a0088d37399a617d43fb2909350a05c991e5
Carlos <110745188+carlDguez19@users.noreply.github.com>
2026-02-26 13:12:15 -0700

    finished exercises for using null section will now do its quiz

--- 08_A_using_null/solutions.sql
+++ 08_A_using_null/solutions.sql
@@ -58,4 +58,49 @@ SELECT name, COALESCE(mobile, '07986 444 2266') AS mobile
 FROM teacher
 
 
--- Exercise 5: 
+-- Exercise 5: Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+
+-- Solution:
+SELECT teacher.name, COALESCE(dept.name, 'None') AS dept
+FROM teacher LEFT JOIN dept
+ON (teacher.dept=dept.id)
+
+
+-- Exercise 6: Use COUNT to show the number of teachers and the number of mobile phones.
+
+-- Solution:
+SELECT COUNT(name), COUNT(mobile) FROM teacher
+WHERE name IS NOT NULL || mobile IS NOT NULL
+
+
+-- Exercise 7: Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+
+-- Solution:
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept
+ON (teacher.dept=dept.id)
+GROUP BY dept.name
+
+
+-- Exercise 8: Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+
+-- Solution:
+SELECT name,
+CASE WHEN dept = 1 || dept = 2
+THEN 'Sci'
+ELSE 'Art'
+END dep
+FROM teacher
+
+
+-- Exercise 9: Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+
+-- Solution:
+SELECT name,
+CASE WHEN dept = 1 || dept = 2
+THEN 'Sci'
+WHEN dept = 3
+THEN 'Art'
+ELSE 'None'
+END dep
+FROM teacher
